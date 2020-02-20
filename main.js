ef5e8a6b1e07f8e0adc2931586d05182b307d0d0
Larissa Martins Mauricio <38192027+martinslm@users.noreply.github.com>
2020-02-20 15:32:15 -0300

    Removendo Menu
    Removendo Menu

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const { app, BrowserWindow, ipcMain } =  require('electron'); 
+const { app, BrowserWindow, ipcMain, Tray, Menu } =  require('electron'); 
 //importando somente alguns modulos do Electron. 
 //app: Este é responsável por controlar o ciclo de vida da aplicação desktop.
 //BrowserWindow: Responsável por criar janelas.
 //IPCMain "escuta" o canal de comunicação que veio do IPCRenderer
 const data = require('./data.js');
+
+let tray = null;
+
 app.on('ready', () => {
-    console.log('Aplicação Iniciada');
+
+    let templateMenu = [{   
+    }]
+    let menuPrincipal = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(menuPrincipal);
+
+    tray = new Tray('./app/img/icon-tray.png');
+
     let mainWindow = new BrowserWindow({
         width: 600,
         height: 400
@@ -19,6 +29,7 @@ app.on('window-all-closed', () => {
 })
 
 let sobreWindow = null;
+
 ipcMain.on('abrir-janela-sobre', () => {
     if(sobreWindow == null) 
     {
